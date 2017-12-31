25ea729889af9679f7e10f31c3063fbb7ef0e2ad
Dima Statz <dima.statz@cloudlock.com>
2017-12-31 22:36:21 +0200

    initial commit

--- scripts/github_reader.py
+++ scripts/github_reader.py
@@ -3,6 +3,7 @@ import requests
 base_url = 'https://api.github.com/'
 
 
+#
 def read_repositories(index=0):
     path = 'repositories?since={}'.format(index)
     result = requests.get(base_url + path)
