66b9292df41b0f64fa4ea3c1dae9f0d5f658273c
Dima Statz <dima.statz@gmail.com>
2018-01-03 15:53:30 +0200

    add docker file

--- scripts/boot.py
+++ scripts/boot.py
@@ -1,6 +1,9 @@
 import time
+import logging
+
+logging.basicConfig(filename='/var/log/visum.log', level=logging.DEBUG)
 
 if __name__ == '__main__':
     while True:
-        print 'boot task'
+        logging.info('boot task is running')
         time.sleep(5)
